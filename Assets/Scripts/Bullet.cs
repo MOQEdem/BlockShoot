ee6fd1f41fa0b48aa68dd18b714d91049694551a
MOQEdem <smirnov_vladimir_svv@inbox.ru>
2022-03-15 00:38:54 +0500

    Finished guns and bullets

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,6 +7,8 @@ public class Bullet : Cartridge
 {
     [SerializeField] private float _speed;
     [SerializeField] private ParticleSystem _flyEffect;
+    [SerializeField] private ParticleSystem _hitEffect;
+    [SerializeField] private float _lifetime;
 
     private Rigidbody _rigidbody;
 
@@ -15,9 +17,24 @@ public class Bullet : Cartridge
         _rigidbody = GetComponent<Rigidbody>();
         _rigidbody.velocity = transform.TransformDirection(Vector3.forward * _speed);
 
+        TransferColorToParticles(_flyEffect);
+        TransferColorToParticles(_hitEffect);
+
+        StartCoroutine(DestroyItself());
+    }
+
+    private void TransferColorToParticles(ParticleSystem particleSystem)
+    {
         Color color = CartridgeRenderer.material.color;
         color.a = 1;
-        var main = _flyEffect.main;
+        var main = particleSystem.main;
         main.startColor = color;
+    }
+
+    private IEnumerator DestroyItself()
+    {
+        yield return new WaitForSeconds(_lifetime);
+
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -2,41 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Gun : MonoBehaviour
 {
     [SerializeField] private List<Cartridge> _magazineOfCartridge;
-    [SerializeField] private List<Color> _ñartridgeColors;
+    [SerializeField] private List<Color> _cartridgeColors;
     [SerializeField] private Transform _muzzle;
     [SerializeField] private Bullet _bullet;
     [SerializeField] private ParticleSystem _shootEffect;
+    [SerializeField] private float _recoilSpeed;
+
+    private bool _isReadyToShoot = true;
+    private Animator _animator;
 
     private void Start()
     {
+        _animator = GetComponent<Animator>();
+
         foreach (var cartridge in _magazineOfCartridge)
         {
-            SetBulletColor(cartridge);
+            SetCartridgeColor(cartridge);
         }
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
-            Shoot();
+        {
+            if (_isReadyToShoot)
+            {
+                Shoot();
+                Reload();
+                PlayAnimation();
+                StartCoroutine(Recoil());
+            }
+        }
     }
 
     private void Shoot()
     {
         Color color = _magazineOfCartridge[0].CartridgeRenderer.material.color;
-        color.a = 1;
-        var main = _shootEffect.main;
-        main.startColor = color;
+        SetEffectColor(color);
         _shootEffect.Play();
         Bullet bullet = Instantiate<Bullet>(_bullet, _muzzle.position, _muzzle.rotation);
         bullet.SetColor(color);
     }
 
-    private void SetBulletColor(Cartridge cartridge)
+    private void SetEffectColor(Color color)
     {
-        cartridge.SetColor(_ñartridgeColors[Random.Range(0, _ñartridgeColors.Count)]);
+        color.a = 1;
+        var main = _shootEffect.main;
+        main.startColor = color;
+    }
+
+    private void SetCartridgeColor(Cartridge cartridge)
+    {
+        cartridge.SetColor(_cartridgeColors[Random.Range(0, _cartridgeColors.Count)]);
+    }
+
+    private void Reload()
+    {
+        for (int i = 0; i < _magazineOfCartridge.Count - 1; i++)
+        {
+            Color color = _magazineOfCartridge[i + 1].CartridgeRenderer.material.color;
+            _magazineOfCartridge[i].SetColor(color);
+        }
+
+        SetCartridgeColor(_magazineOfCartridge[_magazineOfCartridge.Count - 1]);
+    }
+
+    private void PlayAnimation()
+    {
+        _animator.SetTrigger(AnimatorGun.Trigger.Shoot);
+    }
+
+    private IEnumerator Recoil()
+    {
+        var _recoilTime = new WaitForSeconds(_recoilSpeed);
+
+        _isReadyToShoot = false;
+
+        yield return _recoilTime;
+
+        _isReadyToShoot = true;
+    }
+
+    public static class AnimatorGun
+    {
+        public static class Trigger
+        {
+            public const string Shoot = nameof(Shoot);
+        }
     }
 }
